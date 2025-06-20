0ecd35bc208ca158d979c5f547f0066d6f1557be
lost saint <sunymon@protonmail.com>
2025-06-19 21:23:49 -0400

    fix time format

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 function startTime(){
     let today = new Date();
-    let hours = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
+    let folkTime = new Date(today.toLocaleString("en-US", {timeZone: "America/New_York"}));
+    let hours = folkTime.getHours();
+    let min = folkTime.getMinutes();
+    let sec = folkTime.getSeconds();
 
     min = checkTime(min);
 
